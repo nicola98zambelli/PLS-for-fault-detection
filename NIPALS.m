a7bc3c1875814ce8a990e3705b2c866cc4fdae1d
Nicola Zambelli <98172442+nicola98zambelli@users.noreply.github.com>
2023-03-14 21:25:38 +0100

    Add files via upload

--- NIPALS.m
+++ NIPALS.m
@@ -30,35 +30,35 @@ Y(101:150,3)=ones(50,1);
 % normalize Y
 Y_mu=mean(Y);
 Y_std=std(Y);
-%Y=normalize(Y);
+Y=normalize(Y);
 %function [T, P, U, Q, W, B, B2] = PLSI (X,Y)
 %
 % Inputs:
-% X     X maTriX
-% Y     Y maTriX
-% !!SCALED!!
+% X     X matrix normaliz
+% Y     Y matrix normaliz
 % Outputs:
 % T     score for X
 % P     loading for X
 % U     score for Y
 % Q     loading for Y
 % B     regression coefficients
-% calculate The size of X and Y
+% B2    regression Matrix
+
 %Initialization
 Ej=X;
 Fj=Y;
 [nX,mX]  =  size(Ej);
 [nY,pY]  =  size(Fj);
 j_max = min([mX,nX]); %until j=min(m,n): rank di X PaG 81(penultima riga)
-nMaxOuter = 10000; %max number of itertion to convergence
+nMaxOuter = 1e3; %max number of itertion to convergence
 a=3; % 0 <= a <= j_max
 for j = 1 : a
     % choose The column of X has The largest square of sum as T.
     % choose The column of Y has The largest square of sum as U.
-    [dummy,Tnum] =  max(diag(Ej'*Ej));
-    [dummy,Unum] =  max(diag(Fj'*Fj));
-    tj_prec = Ej(:,Tnum); %any column of X
-    uj = Fj(:,Unum);      %any column of Y
+    [~,Tnum] =  max(diag(Ej'*Ej));
+    [~,Unum] =  max(diag(Fj'*Fj));
+    tj_prec = Ej(:,Tnum);
+    uj = Fj(:,Unum);
     % iTeraTion for outer modeling
     for i = 1 : nMaxOuter
         wj = Ej' * uj/ norm(Ej' * uj);
@@ -88,13 +88,13 @@ for j = 1 : a
     B(j,j)= bj;
     B2= W*pinv(P'*W)*pinv(T'*T)*T'*Y;
     % check for residual to stop:
-    if (norm(Ej) ==0 || norm(Fj) ==0)
+    if (norm(Ej) ==0 && norm(Fj) ==0)
         break
     end
 end
 
 Y_hat=X*B2;
-%fo each row set the higer value to 1 and the others to 0 according to 6.4
+%for each row set the higer value to 1 and the others to 0 according to 6.4
 for r=1:n
     max_row=max(Y_hat(r,:));
     for c=1:3
@@ -108,7 +108,7 @@ end
 
 % show performance
 disp('_____________ SHOW PERFORMANCE_______________')
-disp('Runing Multiclass confusionmat')
+disp('Runing Multiclass confusion matrix')
 Y_class=zeros(nY,1);
 Y_class(51:100)=1;
 Y_class(101:150)=2;
@@ -117,13 +117,13 @@ for r=1:n
     if Y_hat(r,2)==1
         Y_hat_class(r)=1;
     else if Y_hat(r,3)==1
-            Y_hat_class(r)=2;
+        Y_hat_class(r)=2;
     end
     end
 end
 [c_matrix,Result,RefereceResult]= confusion.getMatrix(Y_class,Y_hat_class);
 
-%plot reducion order result
+%plot reducion order X in a dimensions
 X_a_reduced=X*P;
 scatter3(X_a_reduced(1:50,1),X_a_reduced(1:50,2),X_a_reduced(1:50,3),'*');
 hold on;
